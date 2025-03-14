8bcc291c395f026d1a3a48704b2fa844c750bf86
samcioli <samcioli2004@gmail.com>
2025-03-14 15:49:31 -0300

    Correcao de rotas no index

--- API/index.js
+++ API/index.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const usuariosRouter = require('./routes/usuarioRoutes');
-const eventosRouter = require('./routes/eventosRoutes');
-const clientesRouter = require('./routes/clientesRoutes');
-const ingressosRouter = require('./routes/ingressosRoutes');
-const vendasRouter = require('./routes/vendasRoutes');
+const usuarioRoutes = require('./routes/usuarioRoutes'); 
+const eventosRoutes = require('./routes/eventoRoutes');
+const clientesRoutes = require('./routes/clienteRoutes');
+const ingressosRoutes = require('./routes/ingressoRoutes');
+const vendasRoutes = require('./routes/vendaRoutes');
 
 const app = express();
 const port = 3000;
@@ -13,11 +13,11 @@ const port = 3000;
 app.use(cors());
 app.use(bodyParser.json());
 
-app.use('/api', usuariosRouter);
-app.use('/api', eventosRouter);
-app.use('/api', clientesRouter);
-app.use('/api', ingressosRouter);
-app.use('/api', vendasRouter);
+app.use('/api', usuarioRoutes);
+app.use('/api', eventoRoutes);
+app.use('/api', clienteRoutes);
+app.use('/api', ingressoRoutes);
+app.use('/api', vendaRoutes);
 
 app.listen(port, () => {
     console.log('Servidor iniciado em http://localhost:' + port);
